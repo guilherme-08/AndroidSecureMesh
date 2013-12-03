213cab3323e03a438c0089aa1988acbd7a1825e5
guilherme-08 <fguilha@hotmail.com>
2013-12-03 18:17:45 +0000

    - Added ReceiveDataThread and SendDataThread

--- AndroidSecureMesh/src/pt/up/fe/ssin/androidsecuremesh/utils/Main.java
+++ AndroidSecureMesh/src/pt/up/fe/ssin/androidsecuremesh/utils/Main.java
@@ -1,36 +1,72 @@
 package pt.up.fe.ssin.androidsecuremesh.utils;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
 	private List<Chat> chatList;
 	private List<User> userList;
-	
-	public Main()
+	private SendDataThread sendDataThread;
+	private ReceiveDataThread receiveDataThread;
+
+	public Main() throws IOException
 	{
 		chatList = new ArrayList<Chat>();
 		userList = new ArrayList<User>();
+		sendDataThread = new SendDataThread();
+		receiveDataThread = new ReceiveDataThread();
+		receiveDataThread.execute();
 	}
-	
+
 	public List<Chat> getChatList() {
 		return chatList;
 	}
 
-	public void addToChatList(Chat chat) {
-		this.chatList.add(chat);
+	public boolean addToChatList(Chat chat) {
+
+		boolean success = true;
+
+		if(!containsInChatList(chat))
+		{
+			this.chatList.add(chat);
+
+			String textToSend = "NewChat," + chat.getName() + "," + chat.getKey();
+			
+			String[] textArray = new String[1];
+			textArray[0] = textToSend;
+			try {
+				if(!sendDataThread.execute(textToSend).get())
+					return false;
+			} catch (InterruptedException e) {
+				return false;
+			} catch (ExecutionException e) {
+				return false;
+			}
+		}
+		else
+		{
+			success = false;
+		}
+
+		return success;
 	}
-	
+
 	public Chat getChatByName(String chatName) {
 		for(Chat chat: chatList)
 			if(chat.getName().equals(chatName))
 				return chat;
 		return null;
 	}
-	
-	
-	
+
+
+
 
 	public List<User> getUserList() {
 		return userList;
@@ -40,11 +76,19 @@ public class Main {
 		User user = new User(userName);
 		this.userList.add(user);
 	}
-	
+
 	public boolean containsInUserList(String username)
 	{
 		for(User user: userList)
 			if(user.getName().equals(username))
+				return true;
+		return false;
+	}
+
+	public boolean containsInChatList(Chat chatName)
+	{
+		for(Chat chat: chatList)
+			if(chat.getName().equals(chatName))
 				return true;
 		return false;
 	}
