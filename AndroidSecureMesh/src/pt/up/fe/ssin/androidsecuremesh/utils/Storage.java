ae64cc3224fa33628fc7a1f9bbf3bdd6531e3e6e
João Anes <joao.anes@gmail.com>
2013-12-04 16:39:33 +0000

    Added IP address get

--- AndroidSecureMesh/src/pt/up/fe/ssin/androidsecuremesh/utils/Storage.java
+++ AndroidSecureMesh/src/pt/up/fe/ssin/androidsecuremesh/utils/Storage.java
@@ -1,5 +1,40 @@
 package pt.up.fe.ssin.androidsecuremesh.utils;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.http.conn.util.InetAddressUtils;
+
 public class Storage {
 	public static User myData = new User("me");
+
+	
+	//shamelessly stolen from http://stackoverflow.com/questions/6064510/how-to-get-ip-address-of-the-device
+	public static String getIPAddress(boolean useIPv4) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress().toUpperCase();
+                        boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr); 
+                        if (useIPv4) {
+                            if (isIPv4) 
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 port suffix
+                                return delim<0 ? sAddr : sAddr.substring(0, delim);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
+	}
+
 }
